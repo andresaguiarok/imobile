b8b16238a10b29eace368b1921f10f9dd8000c3c
Andres Aguiar <aguiarandresok@outlook.es>
2022-09-07 18:49:21 -0300

    pre entrega 1

--- js/index.js
+++ js/index.js
@@ -57,16 +57,19 @@ console.log(samsung);
 //Desafio
 
 let productos = [
-    {nombre: "Iphone 11" , precio: 230000 },
-    {nombre: "Iphone 12" , precio: 530000 },
-    {nombre: "Iphone 13" , precio: 380000 },
-    {nombre: "Motorola G200" , precio: 110000 },
-    {nombre: "Motorola Edge 20 Pro" , precio: 130000 },
-    {nombre: "Motorola Edge 30 Pro" , precio: 170000 },
-    {nombre: "Samsung Galaxy S22 Ultra" , precio: 268000 },
-    {nombre: "Samsung Galaxy Z Flip 3" , precio: 220000 },
-    {nombre: "Samsung Galaxy Z Fold 4" , precio: 410000 },
+    {nombre: "Iphone 11" , precio: 230000},
+    {nombre: "Iphone 12" , precio: 530000},
+    {nombre: "Iphone 13" , precio: 380000},
+    {nombre: "Motorola G200" , precio: 110000},
+    {nombre: "Motorola Edge 20 Pro" , precio: 130000},
+    {nombre: "Motorola Edge 30 Pro" , precio: 170000},
+    {nombre: "Samsung Galaxy S22 Ultra" , precio: 268000},
+    {nombre: "Samsung Galaxy Z Flip 3" , precio: 220000},
+    {nombre: "Samsung Galaxy Z Fold 4" , precio: 410000},
 ];
+
+productos.push({nombre: "Iphone 14", precio: 610000});
+productos.push({nombre: "Samsung Galaxy S21", precio: 112000});
 
 for (let producto of productos){
     let ingrese = prompt("Ingrese el nombre del producto");
@@ -74,4 +77,5 @@ for (let producto of productos){
 
     let mensaje = `El producto seleccionado es ${producto.nombre} y cuesta ${producto.precio} AR$`;
     alert(mensaje);
+    break;
 };
