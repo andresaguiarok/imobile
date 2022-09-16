3bf7436db23a46dceaa6a77ed606c7859cbe48d2
Andres Aguiar <aguiarandresok@outlook.es>
2022-09-15 23:27:47 -0300

    carrito en el html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//Dom , muestra las cards de la tienda
+
 let contenedor = document.getElementById ("tienda");
 
 for (let producto of productos) {
@@ -6,18 +8,20 @@ for (let producto of productos) {
     div.innerHTML = `<div class="card" style="width: 20rem;">
                             <h5 class="card-title text-center">${producto.categoria}</h5>
                             <img src="${producto.img}" class="card-img-top" alt="...">
-                            <div class="card-body">
+                            <div class="card-body d-grid gap-2 ">
                                 <h5 class="card-title text-center">${producto.nombre}</h5>
-                                <p class="card-text text-center">Precio: ${producto.precio}$</p>
-                                <button class="btn btn-primary align-items-center" id=boton${producto.id}>Añadir al carrito</button>
+                                <h6 class="card-text text-center">Precio: ${producto.precio}$</h6>
+                                <button class="btn btn-primary" id=boton${producto.id}>Añadir al carrito</button>
                             </div>
                             </div>`
      contenedor.append(div)
 
      let boton = document.getElementById (`boton${producto.id}`)
+
      boton.addEventListener("click" , () => {
-        boton.innerHTML =`<div class="modal-dialog modal-sm text-bg-success">
-        Se agrego el producto ${producto.nombre} al carrito.
-        </div>`
+        boton.innerHTML =`<div class="modal-dialog modal-sm text-bg-success p-1">
+        <b>Se agrego el producto ${producto.nombre} al carrito.</b>
+        </div>`  
      })
 };
+
